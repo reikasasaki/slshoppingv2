c28bf6bc29299d3040216cd912782fa7e9e38467
reikasasaki <148754851+reikasasaki@users.noreply.github.com>
2023-12-27 15:21:31 +0900

    増減ボタン実装中

--- slshop-frontend/src/main/resources/static/js/calculation.js
+++ slshop-frontend/src/main/resources/static/js/calculation.js
@@ -2,11 +2,34 @@
  * 
  */
 
-function updateResult(index) {
-        var price = document.getElementById("price" + index).textContent;
-        var quantity = document.getElementById("quantity" + index).value;
-        var resultElement = document.getElementById("result" + index);
+//function updateResult(index) {
+//        var price = document.getElementById("price" + index).textContent;
+//        var quantity = document.getElementById("quantity" + index).value;
+//        var resultElement = document.getElementById("result" + index);
+//
+//        var result = parseFloat(price) * parseInt(quantity);
+//        resultElement.textContent = result;
+//    
 
-        var result = parseFloat(price) * parseInt(quantity);
-        resultElement.textContent = result;
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll('.btn-secondary');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var parentTd = button.closest('td');
+            var quantityInput = parentTd.querySelector('.form-control');
+
+            if (button.id === 'decrement_') {
+                let currentNumber = parseInt(quantityInput.value);
+                quantityInput.value = Math.max(currentNumber - 1, 1);
+            } else if (button.id.startsWith('increment_')) {
+                let currentNumber = parseInt(quantityInput.value);
+                quantityInput.value = Math.min(currentNumber + 1, 10);
+            }
+        });
+    });
+});
+
+
+
+
